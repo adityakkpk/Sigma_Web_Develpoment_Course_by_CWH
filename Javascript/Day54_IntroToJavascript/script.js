ade17dcd95bc88c9ea82926e8d7f2a6883a39d5b
Aditya <akkpk933@gmail.com>
2023-11-18 21:46:12 +0530

    javascript Started

---
+++ Javascript/Day54_IntroToJavascript/script.js
@@ -0,0 +1,16 @@
+alert("Hello!");
+
+console.log("Hey i am console...");
+
+var a = prompt("Enter your name");
+console.log("Your name is "+a);
+
+var isTrue =  confirm("Are you sure you want to exit");
+
+if(isTrue){
+    console.log("Welcome Again!")
+}else{
+    console.log("Bho")
+}
+
+document.title = "Kya hal h"
