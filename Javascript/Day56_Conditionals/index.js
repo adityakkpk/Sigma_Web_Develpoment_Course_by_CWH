6d31f8cd58cac251bdc7d280764ed5f7daa4f359
Aditya <akkpk933@gmail.com>
2023-11-21 22:11:03 +0530

    Day56 Conditionals

---
+++ Javascript/Day56_Conditionals/index.js
@@ -0,0 +1,69 @@
+console.log("Hello, Let's study Operators and conditionals!");
+console.log("---------------------------------------------------");
+console.log();
+
+let age = 18;
+let grace = 2;
+
+
+// Operators
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+// console.log(age+=grace);
+// console.log(age!=18);//true
+
+//ternary Operator
+// let a = 6;
+// let b = 8;
+// console.log(a>b? a-b : b-a);
+
+// if(age!=18){
+//     console.log("You cannot Drive.");
+// }
+
+// == evaluate value only
+// if(age==18){
+//     console.log("You can Drive.");
+// }
+// === evaluate value and type both
+// if(age===18){
+//     console.log("You cannot Drive.");
+// }
+
+
+// console.log(age==18 && 18==age);//true
+// console.log(age!=18 || 20!=age);//true
+// console.log(! 20!=age);//true
+
+
+
+
+// Conditions
+
+//If statement
+// if(age==18){
+//     console.log("You can Drive.");
+// }
+
+//If-Else statement
+// if((age+grace)>18){
+//     console.log("You can Drive.");
+// }else {
+//     console.log("You cannot drive.");
+// }
+
+// If Else If Else ladder
+// if(age >= 18){
+//     console.log("You can Drive");
+// }else if(age == 0){
+//     console.log("Are you kidding.");
+// }else {
+//     console.log("You cannot Drive");
+// }
+
+
