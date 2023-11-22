2805efd8dc9f05f12a462bc2b34347684c93d2b2
Aditya <akkpk933@gmail.com>
2023-11-22 22:42:44 +0530

    Day57 done

---
+++ Javascript/Day57_Loops/index.js
@@ -0,0 +1,41 @@
+console.log("Lets learn Loops!");
+console.log("------------------");
+
+let a = 1;
+
+
+//Basic for-Loop 
+// for (let index = 0; index < 10; index++) {
+//     console.log(a + index);
+// }
+
+//for-in Loop
+// let obj = {
+//     name : "Aditya",
+//     role : "Developer",
+//     company : "Google"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key, element);
+// }
+
+// forof loop
+// for (const c of "Aditya") {
+//     console.log(c)
+// }
+
+// While loop
+// let i = 1;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+// Do-while loop
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++;
+// }while (i<6);
