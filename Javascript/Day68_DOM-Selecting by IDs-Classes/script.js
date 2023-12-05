9da6cb11947df085abc79c6cb1bb830d04ea791e
Aditya <akkpk933@gmail.com>
2023-12-06 00:02:43 +0530

    DOM element selector methods

---
+++ Javascript/Day68_DOM-Selecting by IDs-Classes/script.js
@@ -0,0 +1,19 @@
+// let boxes = document.getElementsByClassName('box');
+// console.log(boxes);
+
+// boxes[2].style.backgroundColor = 'red';
+
+// document.getElementById('redbox').style.backgroundColor = 'red';
+
+// document.querySelector('.box').style.backgroundColor = 'green';
+
+// document.querySelectorAll('.box').forEach(element => {
+//     element.style.backgroundColor = 'green';
+// });
+
+// console.log(document.getElementsByTagName('div'));
+
+e = console.log(document.getElementsByTagName('div'));
+
+// console.log(e[4].matches('#redbox'));//true
+
