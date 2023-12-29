e0e66623e23cf13ac2038a73a4f7aab76d11c8a5
Aditya <akkpk933@gmail.com>
2023-12-29 20:13:56 +0530

    Song name added

--- Video-Day84-SpotifyClone/Scripts/script.js
+++ Video-Day84-SpotifyClone/Scripts/script.js
@@ -1,1 +1,42 @@
-console.log('lets write JS');
+async function getSongs(){
+    let a = await fetch('http://127.0.0.1:3000/Video-Day84-SpotifyClone/Songs/');
+    let response = await a.text();
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    let aElms = div.getElementsByTagName('a');
+    let songs = [];
+    for (let index = 0; index < aElms.length; index++) {
+        const element = aElms[index];
+        if (element.href.endsWith('.mp3')) {
+            songs.push(element.href.split('/Songs/')[1]);            
+        }        
+    }
+    return songs;
+}
+
+async function main (){
+    let songs = await getSongs();
+
+    let songUL = document.querySelector('.songList ul');
+
+    for (const song of songs) {
+        songUL.innerHTML += `
+        <li>
+            <img class="invert" src="Images/music.svg" alt="">
+            <div class="info">
+                <div class="songName">${song.replaceAll('%20', ' ')}</div>
+            </div>
+            <div class="playNow">
+                <span>Play Now</span>
+                <img class="invert" src="Images/playSong.svg" alt="">
+            </div>
+        </li>
+        
+        `;
+    }
+
+    let audio = new Audio(songs[0]);
+    // audio.play();
+}
+
+main();
