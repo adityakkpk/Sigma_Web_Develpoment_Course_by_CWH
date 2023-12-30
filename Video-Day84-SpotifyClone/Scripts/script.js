e35462095f972984a06a7bfb124a2b34f555a915
Aditya <akkpk933@gmail.com>
2023-12-30 17:11:42 +0530

    Started playing songs

--- Video-Day84-SpotifyClone/Scripts/script.js
+++ Video-Day84-SpotifyClone/Scripts/script.js
@@ -1,3 +1,5 @@
+let currentSong = new Audio();
+
 async function getSongs(){
     let a = await fetch('http://127.0.0.1:3000/Video-Day84-SpotifyClone/Songs/');
     let response = await a.text();
@@ -14,6 +16,12 @@ async function getSongs(){
     return songs;
 }
 
+const playMusic = (songName) => {
+    currentSong.src = "Songs/" + songName;
+    currentSong.play();
+    play.src = 'Images/pause.svg';
+}
+
 async function main (){
     let songs = await getSongs();
 
@@ -22,8 +30,8 @@ async function main (){
     for (const song of songs) {
         songUL.innerHTML += `
         <li>
+        <div class="info">
             <img class="invert" src="Images/music.svg" alt="">
-            <div class="info">
                 <div class="songName">${song.replaceAll('%20', ' ')}</div>
             </div>
             <div class="playNow">
@@ -35,8 +43,24 @@ async function main (){
         `;
     }
 
-    let audio = new Audio(songs[0]);
-    // audio.play();
+    //Attach an eventListeiner on each song of library
+    Array.from(document.querySelector('.songList').getElementsByTagName('li'))
+      .forEach( e => {
+        e.addEventListener('click', () => {
+            playMusic(e.getElementsByTagName('div')[1].innerHTML);
+        });
+      });
+
+    //Attach event on play, next and prevous
+    play.addEventListener('click', () => {
+        if(currentSong.paused){
+            currentSong.play();
+            play.src = 'Images/pause.svg';
+        }else {
+            currentSong.pause();
+            play.src = 'Images/playSong.svg';
+        }
+    });
 }
 
 main();
