2ed6d5cf8a1b9c6025810829bd5dc3b419a3b05d
Aditya <akkpk933@gmail.com>
2023-12-31 14:56:45 +0530

    Javascript Done

--- Video-Day84-SpotifyClone/Scripts/script.js
+++ Video-Day84-SpotifyClone/Scripts/script.js
@@ -1,5 +1,19 @@
 let currentSong = new Audio();
 
+function secondsToMinutes(seconds){
+    if(isNaN(seconds) || seconds < 0){
+        return 'invalid Input';
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+    
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs(){
     let a = await fetch('http://127.0.0.1:3000/Video-Day84-SpotifyClone/Songs/');
     let response = await a.text();
@@ -16,14 +30,20 @@ async function getSongs(){
     return songs;
 }
 
-const playMusic = (songName) => {
+const playMusic = (songName, pause=false) => {
     currentSong.src = "Songs/" + songName;
-    currentSong.play();
-    play.src = 'Images/pause.svg';
+    if(!pause){
+        currentSong.play();
+        play.src = 'Images/pause.svg';
+    }
+    document.querySelector('.songinfo').innerHTML = decodeURI(songName);
+    document.querySelector('.songtime').innerHTML = '00:00 / 00:00';
 }
 
 async function main (){
     let songs = await getSongs();
+
+    playMusic(songs[0], true);
 
     let songUL = document.querySelector('.songList ul');
 
@@ -60,6 +80,20 @@ async function main (){
             currentSong.pause();
             play.src = 'Images/playSong.svg';
         }
+    });
+
+    //listein for timeupdate event
+    currentSong.addEventListener('timeupdate', ()=>{
+        document.querySelector('.songtime').innerHTML = `${secondsToMinutes(currentSong.currentTime)} / ${secondsToMinutes(currentSong.duration)}`;
+        document.querySelector('.circle').style.left = ((currentSong.currentTime / currentSong.duration)) * 100 + "%";
+    });
+
+    //add Eventlisteiner on seekbar
+    document.querySelector('.seekbar').addEventListener('click', (e) => {
+        // console.log(e.offsetX, e.target.getBoundingClientRect().width);
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+        document.querySelector('.circle').style.left = percent + '%';
+        currentSong.currentTime = ((currentSong.duration )* percent) / 100;
     });
 }
 
