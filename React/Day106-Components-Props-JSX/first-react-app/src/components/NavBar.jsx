b5bd8af4e1eaf2c946ba087d3349745e19c77ef2
Aditya <akkpk933@gmail.com>
2024-01-30 19:16:10 +0530

    Made a basic Navbar using React

---
+++ React/Day106-Components-Props-JSX/first-react-app/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
