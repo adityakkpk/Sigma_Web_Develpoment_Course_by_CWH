fec6dbdb2cf0f860434df7c082c8face82e1d398
Aditya <akkpk933@gmail.com>
2024-02-02 00:40:04 +0530

    Use cases of useRef in React

--- React/Day109-useRef-Hook/src/App.jsx
+++ React/Day109-useRef-Hook/src/App.jsx
@@ -16,10 +16,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -35,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button onClick={()=>{btnRef.current.style.display = "none"}}>Change me</button>
     </>
   )
 }
