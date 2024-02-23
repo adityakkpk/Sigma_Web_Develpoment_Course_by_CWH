8c91d37536d703f70319cd9586eab895574379f0
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-02-23 14:24:40 +0530

    Started Learning Next.Js

---
+++ NextJs/Day121-NextJs/first/app/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Problems Solved By Next.js</h1>
+      <ul>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Additional Features like Router from next nevigation</li>
+        <li>Optimised Rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
+
