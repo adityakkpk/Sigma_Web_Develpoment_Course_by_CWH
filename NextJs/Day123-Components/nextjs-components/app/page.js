407387721800a9410f93e70c0ada15ccb239fcac
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-02-25 18:05:34 +0530

    Image component

--- NextJs/Day123-Components/nextjs-components/app/page.js
+++ NextJs/Day123-Components/nextjs-components/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center">
-      I am Home
+    <div className="container mx-auto flex justify-center items-center">
+      <Image height={100} width={100} src="https://soliloquywp.com/wp-content/uploads/2018/11/nb_ist_cover.jpg" alt="Image description" />
     </div>
   );
 }
