9c07fec9456917a1f989c17ef8a5c5c3f99162a9
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-02-25 15:37:08 +0530

    Link tag and Title change on each page

---
+++ NextJs/Day123-Components/nextjs-components/app/contact/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      I am Contact
+    </div>
+  )
+}
+
+export default contact
+
+export const metadata = {
+  title: "Contact us - Facebook - Connect with the world",
+  description: "This is contact Facebook and we can connect with the world with facebook",
+};
