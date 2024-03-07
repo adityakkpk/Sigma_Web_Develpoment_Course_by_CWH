2cc992da4af73cd12e90a4fc650c683f29c3971c
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-03-07 12:48:50 +0530

    layouts in NextJs

---
+++ NextJs/day129-layout/app/(admin)/adminlogout/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Logged Out
+    </div>
+  )
+}
+
+export default page
---
+++ NextJs/day129-layout/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      About facebook
+    </div>
+  )
+}
+
+export default page
